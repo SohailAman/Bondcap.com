d14f90a4c12725dc66de3f360f0454d9fd3ab351
SohailAman <101714964+SohailAman@users.noreply.github.com>
2022-07-25 22:45:19 +0500

    initial

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// -------
+
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    speed: 1000,
+    mousewheel: true,
+    keyboard: {
+        enabled: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+    },
+});
+
+// -________-
